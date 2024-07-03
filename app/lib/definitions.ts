88bef2d7b388abd01977c7ea95ef627766500574
Dilovar <dilovarmizrobov@gmail.com>
2024-07-03 17:56:01 +0500

    init

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -68,7 +68,7 @@ export type CustomersTableType = {
 export type FormattedCustomersTable = {
   id: string;
   name: string;
-  email: string;
+  email: string;pnpm
   image_url: string;
   total_invoices: number;
   total_pending: string;
